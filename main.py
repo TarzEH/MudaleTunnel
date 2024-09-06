fec415d0d72371d273a7dcb6a2ecc9ef5e08d4df
רפאל אמיתי <refael.amitai@moh.gov.il>
2024-09-06 11:50:14 +0300

    update

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import optparse
     
 def cli_menu():
     parser = optparse.OptionParser()
-    parser.add_option("-sp", "--sourceport",dest="source_port",help="Source port that reflect the target service")
-    parser.add_option("-dp", "--sourceport",dest="destenation_port",help="Destenation port where is the target service")
-    parser.add_option("-t", "--target",dest="target",help="Victim IP")
+    parser.add_option("-s", "--sourceport",dest="source_port",help="Source port that reflect the target service",)
+    parser.add_option("-d", "--destenationport",dest="destenation_port",help="Destenation port where is the target service",)
+    parser.add_option("-t", "--target",dest="target",help="Victim IP",)
+
+if __name__ == "__main__":
+    cli_menu()
