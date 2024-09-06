48cfa37a82d5b75dbc7457122777b75391fccd90
TarzEH <70447976+TarzEH@users.noreply.github.com>
2024-09-06 12:02:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from colorama import Fore
 import optparse
 
+
 def cli_menu():
     parser = optparse.OptionParser(
         usage="Usage: %prog [options]",
-        description="This is a simple CLI tool to specify ports and target."
+        description="This is a simple CLI tool to specify ports and target.",
     )
-    
-    parser.add_option("-s", "--sourceport", dest="source_port", help="Source port that reflects the target service")
-    parser.add_option("-d", "--destenationport", dest="destenation_port", help="Destination port where the target service is")
+
+    parser.add_option(
+        "-s",
+        "--sourceport",
+        dest="source_port",
+        help="Source port that reflects the target service",
+    )
+    parser.add_option(
+        "-d",
+        "--destenationport",
+        dest="destenation_port",
+        help="Destination port where the target service is",
+    )
     parser.add_option("-t", "--target", dest="target", help="Victim IP")
 
-
     (options, args) = parser.parse_args()
-
     print(options)
     return options, args
 
+
 if __name__ == "__main__":
-   print(Fore.GREEN)
-   (options, args) = cli_menu()
+    print(Fore.GREEN)
+    (options, args) = cli_menu()
