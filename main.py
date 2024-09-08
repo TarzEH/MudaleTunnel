5a5bdfe637f986f83ef0450e92bd4b3edc012b89
Refael <refa3211@gmail.com>
2024-09-08 22:51:51 +0300

    add typer lib

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from colorama import Fore
-import optparse
-import time 
 import sys
+import time
+from click import prompt
+import typer
+from typing_extensions import Annotated
+
 
 def writer(text, delay):
     for row in text:
@@ -10,18 +12,8 @@ def writer(text, delay):
             sys.stdout.flush()
             time.sleep(delay)
             
-            
-            
-lorem = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus euismod suscipit. In hac habitasse
-platea dictumst. Ut urna mauris, pharetra in consequat sit amet, tincidunt et arcu. Phasellus quis ullamcorper neque. Sed id sem
-sapien. Vivamus ut diam ultrices tortor maximus ullamcorper vitae at dolor. Proin nec nisi erat. Aliquam sed molestie arcu, nec 
-pharetra massa. Pellentesque nec volutpat nibh, sed ultricies nibh. Suspendisse pretium pulvinar molestie. Proin purus augue, 
-pharetra ut cursus ut, sollicitudin id velit.
-"""
-
-def cli_menu():
-    open_ascii = r"""
-    
+def printer_menu():
+    open_ascii = r"""   
  ▄▄       ▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄        ▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄                
 ▐░░▌     ▐░░▌▐░▌       ▐░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░▌      ▐░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░▌               
 ▐░▌░▌   ▐░▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌▐░▌░▌     ▐░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌               
@@ -34,53 +26,34 @@ def cli_menu():
 ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     
  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀      
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+    
+    
+    
+    
+    
+    
  
  """
 
     print(chr(27) + "[2J")
     writer(open_ascii, delay=0.0010)
 
-    parser = optparse.OptionParser(
-        usage="Usage: %prog [options]",
-        description="This is a simple CLI tool to specify ports and target.",
-    )
-
-    parser.add_option(
-        "-s",
-        "--sourceport",
-        dest="source_port",
-        help="Source port that reflects the target service",
-    )
-    parser.add_option(
-        "-d",
-        "--destenationport",
-        dest="destenation_port",
-        help="Destination port where the target service is",
-    )
-    parser.add_option("-t", "--target", dest="target", help="Victim IP")
-
-    (options, args) = parser.parse_args()
-    print(options)
-    return options, args
 
 
-
+#the menu 
+def cli_menu(
+    src_port: Annotated[int, typer.Option("-s","--srcport", prompt=True, help="Source port that reflects the target service")],
+    dst_port: Annotated[int, typer.Option("-d","--dstport", prompt=True, help="Destination port where the target service is")],
+    target: Annotated[str, typer.Option("-t", "--target", prompt=True ,help="Victim IP")]
+):
     
     
+    
+    
+    print(src_port, dst_port)
+
 
 if __name__ == "__main__":
-    print(Fore.GREEN,)
-    (options, args) = cli_menu()
-    
-    # writer(lorem)
+    printer_menu()
+    typer.run(cli_menu)
