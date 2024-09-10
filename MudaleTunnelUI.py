c683594c701701126df1b69b7629a8a174ea6c9f
TarzEH <70447976+TarzEH@users.noreply.github.com>
2024-09-10 09:53:15 +0300

    create class

---
+++ MudaleTunnelUI.py
@@ -0,0 +1,96 @@
+import socket
+import sys
+import typer
+import time
+import typer
+from typing_extensions import Annotated
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.progress import track
+
+
+class MudaleTunnelUI:
+
+    def __init__(self):
+        self.myip = [
+            (s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())
+            for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]
+        ][0][1]
+
+    def writer(self, text, delay):
+        for row in text:
+            for char in row:
+                sys.stdout.write(char)
+                sys.stdout.flush()
+                time.sleep(delay)
+
+    def printer_menu(self):
+        open_ascii = r"""   
+        ▄▄       ▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄        ▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄                
+        ▐░░▌     ▐░░▌▐░▌       ▐░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░▌      ▐░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░▌               
+        ▐░▌░▌   ▐░▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌▐░▌░▌     ▐░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌               
+        ▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌▐░▌    ▐░▌▐░▌          ▐░▌               
+        ▐░▌ ▐░▐░▌ ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌               
+        ▐░▌  ▐░▌  ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌     ▐░▌     ▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌               
+        ▐░▌   ▀   ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌     ▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌               
+        ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌     ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌    ▐░▌▐░▌▐░▌          ▐░▌               
+        ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      
+        ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     
+        ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀      
+
+
+
+
+
+
+        """
+        print(chr(27) + "[2J")
+        self.writer(open_ascii, delay=0.00010)
+
+    # progerss bar
+    def probar(self):
+        total = 0
+        for value in track(range(100), description="in progerss.."):
+            time.sleep(0.01)
+            total += 1
+        print(f"Processed {total} things")
+
+    # the menu
+    def cli_menu(
+        self,
+        src_port: Annotated[
+            int,
+            typer.Option(
+                "-s",
+                "--srcport",
+                prompt=True,
+                help="Source port that reflects the target service",
+            ),
+        ],
+        dst_port: Annotated[
+            int,
+            typer.Option(
+                "-d",
+                "--dstport",
+                prompt=True,
+                help="Destination port where the target service is",
+            ),
+        ],
+        target: Annotated[
+            str, typer.Option("-t", "--target", prompt=True, help="Victim IP")
+        ],
+    ):
+        self.spinnersquare(
+            descriptiontask=f"connecting to {target} at port: {dst_port}"
+        )
+
+    def spinnersquare(
+        self, descriptiontask: str, delay: float = 5, final_message: str = "Done!"
+    ):
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            transient=False,
+        ) as progress:
+            progress.add_task(description=f"{descriptiontask}...", total=None)
+            time.sleep(delay)
+        print(f"{final_message}")
--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import sys
-import time
-from click import prompt
 import typer
-from typing_extensions import Annotated
-from rich.progress import track
-from graphic import printer_menu, writer, probar, spinnersquare
-
-import socket
-myip = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
-
-
-
-#the menu 
-def cli_menu(
-    src_port: Annotated[int, typer.Option("-s","--srcport", prompt=True, 
-                                          help="Source port that reflects the target service")],
-    dst_port: Annotated[int, typer.Option("-d","--dstport", prompt=True, 
-                                          help="Destination port where the target service is")],
-    target: Annotated[str, typer.Option("-t", "--target", prompt=True ,help="Victim IP")]
-):
-    spinnersquare(descriptiontask=f"connecting to {target} at port: {dst_port}")
+from MudaleTunnel.MudaleTunnelUI import MudaleTunnelUI
 
 
 if __name__ == "__main__":
-    printer_menu()
+    ui = MudaleTunnelUI()
+    ui.printer_menu()
     # typer.run(probar)
-    typer.run(cli_menu)
+    typer.run(ui.cli_menu)
