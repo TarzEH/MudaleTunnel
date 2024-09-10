f71ed8fa6949c73a09a203b3d445e8498b468c66
TarzEH <70447976+TarzEH@users.noreply.github.com>
2024-09-10 10:14:39 +0300

    main

--- MudaleTunnelUI.py
+++ MudaleTunnelUI.py
@@ -1,11 +1,10 @@
 import socket
-import sys
-import typer
 import time
 import typer
 from typing_extensions import Annotated
-from rich.progress import Progress, SpinnerColumn, TextColumn
-from rich.progress import track
+from rich import print
+from rich.progress import Progress, SpinnerColumn, TextColumn,track
+
 
 
 class MudaleTunnelUI:
@@ -15,16 +14,7 @@ class MudaleTunnelUI:
             (s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())
             for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]
         ][0][1]
-
-    def writer(self, text, delay):
-        for row in text:
-            for char in row:
-                sys.stdout.write(char)
-                sys.stdout.flush()
-                time.sleep(delay)
-
-    def printer_menu(self):
-        open_ascii = r"""   
+        self.logo=r"""   
         ▄▄       ▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄        ▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄                
         ▐░░▌     ▐░░▌▐░▌       ▐░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░▌      ▐░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░▌               
         ▐░▌░▌   ▐░▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌▐░▌░▌     ▐░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌               
@@ -43,16 +33,6 @@ class MudaleTunnelUI:
 
 
         """
-        print(chr(27) + "[2J")
-        self.writer(open_ascii, delay=0.00010)
-
-    # progerss bar
-    def probar(self):
-        total = 0
-        for value in track(range(100), description="in progerss.."):
-            time.sleep(0.01)
-            total += 1
-        print(f"Processed {total} things")
 
     # the menu
     def cli_menu(
@@ -79,6 +59,8 @@ class MudaleTunnelUI:
             str, typer.Option("-t", "--target", prompt=True, help="Victim IP")
         ],
     ):
+        print("[bold blue]"+self.logo)
+        print("[bold red]"+self.myip)
         self.spinnersquare(
             descriptiontask=f"connecting to {target} at port: {dst_port}"
         )
--- main.py
+++ main.py
@@ -4,6 +4,4 @@ from MudaleTunnelUI import MudaleTunnelUI
 
 if __name__ == "__main__":
     ui = MudaleTunnelUI()
-    ui.printer_menu()
-    # typer.run(probar)
     typer.run(ui.cli_menu)
